50fb9b6647654422b30231fa3506b50669c89367
1000jaeh <1000jaeh@gmail.com>
2018-08-22 19:37:45 +0900

    polish

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,12 +1,3 @@
 INSERT INTO promotions (id, category, content, promotion_month)
 VALUES
-	(1,'action',62,1),
-	(2,'comedy',19,2),
-	(3,'drama',903,3),
-	(4,'family_kids',41,4),
-	(5,'horror',905,5),
-	(6,'korean',41,6),
-	(7,'romance',48,7),
-	(8,'sf_fantasy',30,8),
-	(9,'tv',908,9),
-	(10,'winner',42,10);
+	(1,'drama',903,3);
